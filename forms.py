3b4ddada96a7374ad6898a44b7c89a89eaae0bbb
YY Ahn <yongyeol@gmail.com>
2020-05-29 00:19:08 -0400

    now pages can be edited

--- forms.py
+++ forms.py
@@ -1,7 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import HiddenField, StringField, TextAreaField
 from wtforms.validators import DataRequired
 
 
 class SearchForm(FlaskForm):
     query = StringField("query", validators=[DataRequired()])
+
+
+class EditForm(FlaskForm):
+    path = HiddenField("path", validators=[DataRequired()])
+    content = TextAreaField("content", validators=[DataRequired()])
+    pagemeta = TextAreaField("pagemeta", validators=[DataRequired()])
--- yyiki.py
+++ yyiki.py
@@ -1,7 +1,11 @@
-from flask import Flask, redirect, render_template, url_for
+import os
+from datetime import datetime
+
+import yaml
+from flask import Flask, redirect, render_template
 from flask_flatpages import FlatPages, pygments_style_defs
 
-from forms import SearchForm
+from forms import EditForm, SearchForm
 
 app = Flask(__name__)
 
@@ -14,6 +18,7 @@ app.config["FLATPAGES_MARKDOWN_EXTENSIONS"] = [
     "markdown_katex",
     "mdx_wikilink_plus",
     "mdx_linkify",
+    "mdx_headdown",
 ]
 app.config["FLATPAGES_EXTENSION_CONFIGS"] = {
     "mdx_wikilink_plus": {"base_url": "/wiki/"}
@@ -24,7 +29,7 @@ pages = FlatPages(app)
 
 @app.route("/")
 def home():
-    return render_template("home.html", page={"title": "Home", "content": "home"})
+    return redirect("/wiki/Home")
 
 
 @app.route("/pygments.css")
@@ -38,20 +43,64 @@ def create_page(path):
 
 
 @app.route("/wiki/<path:path>/")
-def page(path):
+def show_page(path):
     page = pages.get_or_404(path)
     form = SearchForm()
     template = page.meta.get("template", "page.html")
     return render_template(template, page=page, form=form)
 
 
+@app.route("/edit/<path:path>/")
+def edit_page(path):
+    page = pages.get(path)
+    form = EditForm()
+    form.path.data = page.path
+    form.content.data = page.body
+    form.pagemeta.data = yaml.dump(page.meta)
+    # filename = "{}.md".format(os.path.join(pages.root, page.path))
+    return render_template("edit.html", page=page, form=form)
+
+
+def write_page(page):
+    filename = "{}.md".format(os.path.join(pages.root, page.path))
+    with open(filename, "w") as f:
+        f.write(yaml.dump(page.meta))
+        f.write("\n")
+        f.write(page.body.replace("\r", ""))
+
+
+@app.route("/save", methods=["GET", "POST"])
+def save_page():
+    form = EditForm()
+    if form.validate_on_submit():
+        page = pages.get(form.path.data)
+        page.body = form.content.data
+        page.meta = yaml.safe_load(form.pagemeta.data)
+        write_page(page)
+    return redirect(f"/wiki/{page.path}")
+
+
 @app.route("/search", methods=["GET", "POST"])
-def search():
+def search_page():
     form = SearchForm()
-    print(form.query)
     if form.validate_on_submit():
         return redirect(f"/wiki/{form.query.data}")
     return redirect("/wiki/Home")
+
+
+@app.route("/list/")
+def page_list():
+    form = SearchForm()
+    articles = []
+    for page in pages:
+        filename = "{}.md".format(os.path.join(pages.root, page.path))
+        mtime = os.path.getmtime(filename)
+        isotime = datetime.fromtimestamp(mtime).strftime("%Y-%m-%d")
+        if "published" in page.meta:
+            articles.append((mtime, isotime, page))
+
+    sorted_article_list = sorted(articles, reverse=True, key=lambda p: p[0])
+    return render_template("list.html", pages=sorted_article_list, form=form)
 
 
 if __name__ == "__main__":
