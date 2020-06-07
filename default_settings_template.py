eb158ff42e73e1bf2c58b1b7c529729becd84cf5
YY Ahn <yongyeol@gmail.com>
2020-06-07 13:25:15 -0400

    adding markdown footnotes extension

--- default_settings_template.py
+++ default_settings_template.py
@@ -1,7 +1,14 @@
-SECRET_KEY = 'your secret key here'
+SECRET_KEY = "your secret key here"
 FLATPAGES_ROOT = "pages"
 FLATPAGES_EXTENSION = ".md"
-FLATPAGES_MARKDOWN_EXTENSIONS = ["codehilite", "markdown_katex", "mdx_wikilink_plus", "mdx_linkify", "mdx_headdown"]
+FLATPAGES_MARKDOWN_EXTENSIONS = [
+    "codehilite",
+    "markdown_katex",
+    "mdx_wikilink_plus",
+    "mdx_linkify",
+    "mdx_headdown",
+    "footnotes",
+]
 FLATPAGES_EXTENSION_CONFIGS = {
     "mdx_wikilink_plus": {
         "base_url": "/wiki/",
